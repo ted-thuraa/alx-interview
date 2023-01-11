d0be10f649220163a10b439b6eee58a2ffe1911e
ted-thuraa <mutwirited@gmail.com>
2023-01-11 11:49:59 +0300

    solved

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Minimum Operations
+'''
+
+def minOperations(n: int) -> int:
+    '''Minimum operations needed to get H characters'''
+
+    nextt = 'H'
+    body = 'H'
+    op_count = 0
+
+    while (len(body)) < n:
+        #current no of h divisible by n without remainder?
+        if n % len(body) == 0:
+            #if so means welldo copy all and paste
+            op_count += 2
+            nextt = body
+            body += body
+        else:
+            #we only do paste what is in clipboard
+            op += 1
+            body += nextt
+        if len(body) != n:
+            return 0
+        return op_count
