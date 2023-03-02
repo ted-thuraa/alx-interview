b676005be07fd27421b3278a58cddb8e1f1dc774
ted-thuraa <mutwirited@gmail.com>
2023-03-02 09:09:30 +0300

    islandPerimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Island perimeter computing module.
+"""
+
+
+def island_perimeter(grid):
+    """Computes the perimeter of an island with no lakes.
+    """
+    visited = set()
+
+    def dfs(i, j):
+        if i >= len(grid) or j >= len(grid[0]) or i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+        if (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+        perim = dfs(i, j + 1)
+        perim += dfs(i + 1, j)
+        perim += dfs(i, j - 1)
+        perim += dfs(i - 1, j)
+        return perim
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]:
+                    return dfs(i,j)
